ea4c0e160be7399a71b912de9f60a1b82cdd7c67
Nir Rattner <nirrattner@gmail.com>
2018-04-10 18:28:43 -0400

    Cleaner resize

--- src/js/library/image-processing/ImageUpdater.js
+++ src/js/library/image-processing/ImageUpdater.js
@@ -7,7 +7,7 @@ export function update(originalImage, colorQuantity, gridColor, stitchesHigh, st
   const cappedStitchesWide = Math.min(stitchesWide, originalImage.bitmap.width);
   const smallImage = originalImage
     .clone()
-    .resize(cappedStitchesWide, cappedStitchesHigh, Jimp.RESIZE_NEAREST_NEIGHBOR);
+    .resize(cappedStitchesWide, cappedStitchesHigh, Jimp.RESIZE_BEZIER);
   const modifiedImage = ColorClusterer.cluster(smallImage, colorQuantity)
     .resize(originalImage.bitmap.width, originalImage.bitmap.height, Jimp.RESIZE_NEAREST_NEIGHBOR);
   return GridRenderer.renderGrid(modifiedImage, gridColor, cappedStitchesHigh, cappedStitchesWide);
